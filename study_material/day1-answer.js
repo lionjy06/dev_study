99da2c0dc8bc1d19420c76d6a2e36f44a2fa25c0
DESKTOP-K0E0GDQ\KSY <sopie1500@gmail.com>
2023-08-29 20:49:34 +0900

    day1-answer commit

---
+++ study_material/day1-answer.js
@@ -0,0 +1,27 @@
+
+//*변수 할당* /
+practiceName = 'soyeon';
+
+practiceAge = 30;
+
+practiceGender = 'female';
+
+practiceHobbies = ['play game', 'watching movie'];
+
+practiceGirlFriend = false;
+
+
+//*속성 추가* /
+//obj.personality = "good"; 
+obj["personality"] = 'good';
+
+//*속성 삭제 */
+//delete obj.age;
+delete obj["age"];
+
+//*속성 수정 */
+//obj.hasGirlFriend = "false";
+obj["hasGirlFriend"] = false;
+
+
+console.log(obj);
